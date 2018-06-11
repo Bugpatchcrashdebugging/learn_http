428582a249c7a3c54cdffd61c788d0c17cacc8b9
karasev-a <karasev.avk@gmail.com>
2018-06-12 00:43:56 +0300

    Made correct adding data of form to json file. Start to make promise

--- server.js
+++ server.js
@@ -2,18 +2,15 @@ let http = require("http");
 let fs = require("fs");
 const qs = require('querystring');
 
-
-let html = fs.readFileSync("index.html");
+let htmlCash = fs.readFileSync("index.html");
 
 http.createServer((request, response) => {
 
     if (request.method === "GET" && request.url === "/") {
         console.log(request.url);
         console.log(request.method);
-        response.end(html);
-    }
-
-    if (request.method === "POST") {
+        response.end(htmlCash);
+    } else if (request.method === "POST") {
         console.log(request.url);
         console.log(request.method);
 
@@ -23,13 +20,25 @@ http.createServer((request, response) => {
             body.push(chunk);
         }).on('end', () => {
             body = Buffer.concat(body).toString();
-            let str = JSON.stringify(qs.parse(body.toString()), null, "\t");
+            let formData = qs.parse(body.toString());
 
-            fs.appendFile("dataOfForm.txt", str, "utf8", (err) => {
-                if (err) throw err;
-                console.log('The "data to append" was appended to file!');
+            fs.readFile("dataOfForm.json", 'utf8', (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    let oldJsonFileObj = JSON.parse(data);
+                    oldJsonFileObj.push(formData);
+                    json = JSON.stringify(oldJsonFileObj);
+                    fs.writeFile("dataOfForm.json", json, 'utf8', (err) => {
+                        if (err) {
+                            throw err;
+                        } else {
+                            console.log("The data of form was append to json file");
+                        }
+                    });
+                }
             });
         });
-        response.end(html);
+        response.end(htmlCash);
     }
 }).listen(3000, () => console.log("Server is working"));
--- serverProm.js
+++ serverProm.js
@@ -1,5 +1,63 @@
 let http = require("http");
 let fs = require("fs");
-const qs = require('querystring');
+//cdconst qs = require('querystring');
 
 
+// let promiseReadFile = (path) => { new Promise (
+//     (res, rej) => {
+//         fs.readFile(path, (err, data) => {
+//             if (err) {
+//                 rej(err);
+//             } else {
+//                 res(data);
+//             }
+//         })
+//     })
+// }
+
+// promiseReadFile("index.html").then(res =>{console.log(res);
+// })
+
+http.createServer((request, response) => {
+    function readFileProm(path, coding) {
+        return new Promise((res, rej) => {
+            fs.readFile(path, coding, (err, data) => {
+                if (err) {
+                    rej(err);
+                } else {
+                    res(data);
+                }
+            })
+        })
+    }
+    
+    //readFileProm("index.html","utf8").then((a)=>{(a)})
+    
+
+    if (request.method === "GET" && request.url === "/") {
+        console.log(request.url);
+        console.log(request.method);
+        readFileProm("index.html","utf8").then((a)=>{(a)}).toString()
+        response.end();
+    }
+
+    // if (request.method === "POST") {
+    //     console.log(request.url);
+    //     console.log(request.method);
+
+    //     let body = [];
+
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk);
+    //     }).on('end', () => {
+    //         body = Buffer.concat(body).toString();
+    //         let str = JSON.stringify(qs.parse(body.toString()), null, "\t");
+
+    //         fs.appendFile("dataOfForm.txt", str, "utf8", (err) => {
+    //             if (err) throw err;
+    //             console.log('The "data to append" was appended to file!');
+    //         });
+    //     });
+    //     response.end(html);
+    // }
+}).listen(3000, () => console.log("Server is working"));
